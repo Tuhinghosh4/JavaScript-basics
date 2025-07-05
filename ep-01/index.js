ca67399327b4090d14cf298a5c16e16f2f838d6e
Tuhinghosh4 <tuhing331@gmail.com>
2025-07-05 11:50:59 +0530

    ep-01

---
+++ ep-01/index.js
@@ -0,0 +1,19 @@
+//Template literals:
+
+const age = 20;
+const fullName = "John Doe";
+
+//output: Hello Mr John doe your age is: 20
+
+console.log("Hello Mr "+fullName+ " your age is: "+age);
+console.log(`
+    Hello 
+    Mr ${fullName} 
+    your age is: ${age}`
+);
+
+//Q. Print sum of 2 numbers
+const a = 10;
+const b = 20;
+console.log("Sum of "+ a +" and " + b + " is: "+ a+b);
+console.log(`Sum of ${a} and ${b} is: ${a+b}`);
