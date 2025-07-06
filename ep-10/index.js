96a44352b14b167bdb94032ed8fd21c376fa227c
Tuhinghosh4 <tuhing331@gmail.com>
2025-07-06 13:33:55 +0530

    new commit

---
+++ ep-10/index.js
@@ -0,0 +1,17 @@
+const age=19;
+if(age>=18){
+    console.log("You are eligible to drive car");  
+}
+else{
+    console.log("You are not eligible to drive car")
+}
+age>=18 ?console.log("You are eligible to drive car"):console.log("You are not eligible to drive car"); 
+const obj = {
+    name:"Patel",
+    address:{
+
+    }
+}
+// console.log(obj.address.city.village);
+const str = false;
+str&&console.log("true");
