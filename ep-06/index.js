27d04df6d2de086eb9c5e9552697f4fead45f5a6
Tuhinghosh4 <tuhing331@gmail.com>
2025-07-06 12:30:05 +0530

    new commit

--- ep-06/index.js
+++ ep-06/index.js
@@ -34,3 +34,23 @@ product.map((element)=>{
 })
 
 //filter
+const arr1 = [10,20,30,40,50];
+const res = arr1.filter((element)=>{
+    return element >= 30;//
+})
+console.log(res);
+
+//reduce
+function sumOfElement(arr){
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+console.log(sumOfElement(arr1));
+
+const ans = arr1.reduce((accumulator,element)=>{
+    return accumulator+element;
+},0)
+console.log(ans)
